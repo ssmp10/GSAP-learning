4efd80c9fa4055b1d7475e62dca9e58acf8ddf8a
ssmp10 <ssmpamj10@gmail.com>
2026-09-15 22:33:48 +0530

    scroll trigger

---
+++ 2nd Vdo/script.js
@@ -0,0 +1,18 @@
+gsap.from("#page1 #box", {
+    scale: 0,
+    delay: 1,
+    duration: 2,
+    rotate: 360
+})
+
+gsap.from("#page2 h1", {
+    opacity:0,
+    duration:2,
+    x:500,
+    scrollTrigger:{
+        trigger: "#page2 h1",
+        scroller: body,
+        markers: true,
+        start: "top 50%"
+    }
+})
