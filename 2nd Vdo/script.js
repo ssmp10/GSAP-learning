495247f2a404232ef9d642b499821e9e0b43b74e
ssmp10 <ssmpamj10@gmail.com>
2026-09-17 03:09:05 +0530

    2nd day

--- 2nd Vdo/script.js
+++ 2nd Vdo/script.js
@@ -1,18 +1,9 @@
-gsap.from("#page1 #box", {
-    scale: 0,
-    delay: 1,
-    duration: 2,
-    rotate: 360
-})
-
-gsap.from("#page2 h1", {
-    opacity:0,
-    duration:2,
-    x:500,
+gsap.to("#page2 h1",{
+    transform:"translateX(-100%)",
     scrollTrigger:{
-        trigger: "#page2 h1",
-        scroller: body,
+        trigger: "#page2",
+        scroller:"body",
         markers: true,
-        start: "top 50%"
+        start:"top 0%"
     }
 })
